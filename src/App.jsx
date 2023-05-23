b6440eec155a42bd12ee87b620fd3579420ddf4d
hks713 <hks713@naver.com>
2023-05-23 15:35:48 +0900

    [DELETED] - legacy jsx delete

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,5 @@
-import React, { useState } from 'react';
-import Header from './components/Header';
-import Todos from './components/Todos';
-import Footer from './components/Footer';
+import React from 'react';
 import TodoList from './components/TodoList/TodoList';
-import AddTodo from './components/AddTodo/AddTodo';
 
 export default function App() {
   return (
@@ -11,30 +7,4 @@ export default function App() {
       <TodoList />
     </div>
   );
-  // const [todos, setTodos] = useState(initData);
-  // const [input, setInput] = useState('');
-  // const handleClick = (e) => {
-  //   e.preventDefault();
-  //   setTodos((prev) => [input, ...prev]);
-  // };
-
-  // const handleChange = (e) => {
-  //   setInput(e.target.value);
-  // };
-
-  // const handleDelete = (e) => {
-  //   console.log(e);
-  // };
-
-  // return (
-  //   <>
-  //     <Header />
-  //     <Todos todos={todos} handleDelete={handleDelete} />
-  //     <Footer
-  //       input={input}
-  //       handleClick={handleClick}
-  //       handleChange={handleChange}
-  //     />
-  //   </>
-  // );
 }
--- src/components/Header.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function Header() {
-  return <div>오늘의 할일</div>;
-}
